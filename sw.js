6fe0fb37b9277dd89d5929842c4cb23320913488
Alan Guillermo Avila Macias <g20160094@gmail.com>
2021-06-14 20:14:59 -0500

    [ADD] Counting application

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_ELEMENTS = [
+  "./",
+  "https://unpkg.com/react@17/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+  "https://unpkg.com/@babel/standalone/babel.min.js",
+  "./style.css",
+  "./components/Counter.js",
+  "./app.js"
+]; // Adding routes that are being used in the system
+
+const CACHE_NAME = "v1_cache_counter_react";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(CACHE_ELEMENTS).then(() => {
+        self.skipWaiting()
+      }).catch(console.log) // Allows you to add to this cache as many routes as you like
+    }) // Allows us to pass any name
+  ); // Wait for something to run
+});
+
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [CACHE_NAME];
+  e.waitUntil(
+    caches.keys().then((cachesNames) => {
+      return Promise.all(cachesNames.map(cachesNames => {
+        return cacheWhitelist.indexOf(cachesNames) === -1 && caches.delete(cachesNames)
+      })) // Resibe un array, puede quitar varias promesas o resolver hambas promesas
+    }).then( () => self.clients.claim()) // Da todas las claves en caso de que se tenga mas de un cache instalado
+  ); // Wait for something to run
+});
+
+self.addEventListener("fetch", (e) => { // Busca nuevas versiones de los archivos y retorna o captura nuevos elementos
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      if (res) {
+        return res;
+      }
+      return fetch(e.request);
+    })
+  );
+});
